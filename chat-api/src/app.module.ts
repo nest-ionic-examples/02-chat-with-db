58fe1201853b6e4ebd592d23c2e3b4ab0ec5446a
luvargas <luvargas@greathealthworks.com>
2019-01-25 13:37:17 -0500

    enhance docs

--- chat-api/src/app.module.ts
+++ chat-api/src/app.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesGateway } from './gateways/messages/messages.gateway';
+// tag::import-messages-controller[]
 import { MessagesController } from './controllers/messages/messages.controller';
+// end::import-messages-controller[]
 import { RoomsController } from './controllers/rooms/rooms.controller';
 import { TypegooseModule } from 'nestjs-typegoose'; // <1>
 import { Message } from './models/message.model';
@@ -14,7 +16,13 @@ import { User } from './models/user.model';
     TypegooseModule.forRoot('mongodb://chat-admin:password123@localhost/chat', {}), // <2>
     TypegooseModule.forFeature(Message, Room, User), // <3>
   ],
-  controllers: [AppController, RoomsController, MessagesController],
+  controllers: [
+    AppController,
+    RoomsController,
+    // tag::messages-controller[]
+    MessagesController,
+    // end::messages-controller[]
+  ],
   providers: [AppService, MessagesGateway],
 })
 export class AppModule {}
